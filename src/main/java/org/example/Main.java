1f0a3d8d55174d01072724efd516eeb3079426f7
GouthamUR <Goutham.UR@iiitb.ac.in>
2024-12-08 22:51:39 +0530

    CHeck webhook

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -34,7 +34,7 @@ public class Main {
                     running = false;
                     break;
                 default:
-                    System.out.println("Invalid option! Please choose a valid operation.");
+                    System.out.println("Invalid option! Choose a valid operation.");
                     break;
             }
         }
